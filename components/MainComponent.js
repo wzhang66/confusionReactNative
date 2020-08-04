2740c2334b301b2ce3b437bf407fb168ae381a33
Weiwei <ww.zhang@mail.utoronto.ca>
2020-08-04 16:07:56 -0400

    adding components to confusion app

---
+++ components/MainComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import DishDetail from './DishDetailComponent';
+import { View } from 'react-native';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null
+    };
+  }
+
+  onDishSelected(dishId){
+      this.setState({selectedDish: dishId});
+  }
+
+  render() {
+    return (
+        <View>
+            <Menu dishes={this.state.dishes} 
+                onPress={(dishId) => this.onDishSelected(dishId)} />
+            <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        </View>
+    );
+  }
+}
+  
+export default Main;
